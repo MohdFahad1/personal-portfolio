2040e814907bf0ebe4d26a5adcfa576f1585db3d
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-05-04 12:25:14 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Education from './pages/Education/Education';
+import Skills from './pages/Skills/Skills';
+import Projects from './pages/Projects/Projects';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/education" element={<Education />}/>
+          <Route path="/skills" element={<Skills />}/>
+          <Route path="/projects" element={<Projects />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
