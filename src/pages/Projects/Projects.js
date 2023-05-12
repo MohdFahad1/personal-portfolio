86ea7e7e55fa1acd0eaeba5e5df70692ad090e8b
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-05-12 17:53:03 +0530

    Project page Styling completed

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,33 +1,38 @@
 import React from 'react'
 import './Projects.css';
 import { data } from './data';
-import { DiGithubBadge } from "react-icons/di";
+import { FiGithub } from "react-icons/fi";
 import { CgLink } from "react-icons/cg";
+
 
 const Projects = () => {
   return (
     <>
-    <h1 style={{textAlign:"center"}}>Projects</h1>
-    <div id='projects'>
-      {data.map(({ id, title, github, demo, logo }) => {
-        return (
-          <div className='main-card'>
-            <div>
-              <div className='card-title'><h1>{title}</h1></div>
-              <div className='card-image'><img src={logo} alt={title} key={id}/></div>
+      <h1 style={{ textAlign: "center" }}>Projects</h1>
+      <div id='projects'>
+        {data.map(({ id, title, github, demo, logo, tech1, tech2, tech3, tech4, }) => {
+          return (
+            <div className='main-card'>
+              <div className='card-info'>
+                <h1>{title}</h1>
+                <img src={logo} alt={title} key={id} />
+              </div>
+              <hr style={{marginTop:"1rem"}}/>
+              <div className='card-tech'><p>Tech Used : {tech1}, {tech2}, {tech3}, {tech4}</p></div>
+              <hr style={{marginBottom:"1rem"}}/>
+              
+              <div className='card-links'>
+                <a href={github} target='_blank' rel="noreferrer">
+                  <FiGithub className='link-icon'/>
+                </a>
+                <a href={demo} target='_blank' rel="noreferrer">
+                  <CgLink className='link-icon'/>
+                </a>
+              </div>
             </div>
-            <div className='card-links'>
-              <a href={github} target='_blank' rel="noreferrer">
-                <DiGithubBadge />
-              </a>
-              <a href={demo} target='_blank' rel="noreferrer">
-                <CgLink />
-              </a>
-            </div>
-          </div>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
     </>
   )
 }
