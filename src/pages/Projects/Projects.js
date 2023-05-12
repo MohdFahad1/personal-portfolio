297e2a4efd64ff1040c90f3e2d5937560235a194
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-05-12 11:03:25 +0530

    Project page basic structure done.

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,9 +1,34 @@
 import React from 'react'
 import './Projects.css';
+import { data } from './data';
+import { DiGithubBadge } from "react-icons/di";
+import { CgLink } from "react-icons/cg";
 
 const Projects = () => {
   return (
-    <div id='projects'>Projects</div>
+    <>
+    <h1 style={{textAlign:"center"}}>Projects</h1>
+    <div id='projects'>
+      {data.map(({ id, title, github, demo, logo }) => {
+        return (
+          <div className='main-card'>
+            <div>
+              <div className='card-title'><h1>{title}</h1></div>
+              <div className='card-image'><img src={logo} alt={title} key={id}/></div>
+            </div>
+            <div className='card-links'>
+              <a href={github} target='_blank' rel="noreferrer">
+                <DiGithubBadge />
+              </a>
+              <a href={demo} target='_blank' rel="noreferrer">
+                <CgLink />
+              </a>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+    </>
   )
 }
 
---
+++ src/pages/Projects/data.js
@@ -0,0 +1,50 @@
+import weather from "../../Assets/snowy.png";
+import pixabay from "../../Assets/pixabay.svg";
+import markdown from "../../Assets/markdown.png";
+import quote from "../../Assets/quote.png"
+export const data = [
+  {
+    id: 1,
+    logo: pixabay ,
+    title: "Pixabay Clone",
+    github: "https://github.com/MohdFahad1/pixabay-clone",
+    demo: "https://react-pixabay-clone.netlify.app/",
+    tech1: "HTML",
+    tech2: "CSS",
+    tech3: "Javascript",
+    tech4: "React",
+  },
+  {
+    id: 2,
+    logo: weather,
+    title: "Weather Website",
+    github: "https://github.com/MohdFahad1/weather-app.netlify",
+    demo: "https://fahad-weather-application.netlify.app",
+    tech1: "HTML",
+    tech2: "CSS",
+    tech3: "Javascript",
+    tech4: "React",
+  },
+  {
+    id: 3,
+    logo: quote ,
+    title: "Random Quote",
+    github: "https://github.com/MohdFahad1/random-quote.netlify",
+    demo: "https://random-quote-generator-application.netlify.app",
+    tech1: "HTML",
+    tech2: "CSS",
+    tech3: "Javascript",
+    tech4: "React",
+  },
+  {
+    id: 4,
+    logo: markdown,
+    title: "MarkDown Editor",
+    github: "https://github.com/MohdFahad1/markdown-editor",
+    demo: "https://vcbz22.csb.app/",
+    tech1: "HTML",
+    tech2: "CSS",
+    tech3: "Javascript",
+    tech4: "React",
+  }
+];
