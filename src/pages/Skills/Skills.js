be08451ddfb63ecfd4a31377a3d83caf1d0b2532
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-05-22 15:16:25 +0530

    favicon added

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -14,13 +14,13 @@ const Skills = () => {
       <h1 style={{ textAlign: "center" }}>Skills</h1>
       <div id='skills'>
         <div className='skills-para'>
-          <p data-aos="fade-up" data-aos-duration="1000">I have learned a lot of skills as a frontend developer and I am always learning something new to add a new skill to the list
+          <p data-aos="fade-right" data-aos-duration="1000">I have learned a lot of skills as a frontend developer and I am always learning something new to add a new skill to the list
           </p>
         </div>
         <div>
           <div className='skills-container'>
             <div className='skills-content'>
-              <h3 className='skills-title' data-aos="fade-up" data-aos-duration="1200">
+              <h3 className='skills-title' data-aos="fade-left" data-aos-duration="1200">
                 <img src={brackets} alt="brackets"/> FrontEnd Developer
               </h3>
               <div className='skills-info'>
