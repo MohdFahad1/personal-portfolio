a6fba7d06954686884c5d831d3b0f4b6ab9d5d24
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-11-12 13:00:04 +0530

    Projects Added

--- src/pages/Projects/data.js
+++ src/pages/Projects/data.js
@@ -1,3 +1,6 @@
+import nike from "../../Assets/nike.png"
+import job from "../../Assets/job.png"
+import unicus from "../../Assets/unicus.png"
 import weather from "../../Assets/snowy.png";
 import pixabay from "../../Assets/pixabay.svg";
 import markdown from "../../Assets/markdown.png";
@@ -5,30 +8,54 @@ import quote from "../../Assets/quote.png"
 export const data = [
   {
     id: 1,
+    logo: nike  ,
+    title: "Nike Store",
+    github: "https://github.com/MohdFahad1/nike-store",
+    demo: "https://nike-store-delta.vercel.app/",
+    tech: "Next JS, Tailwind CSS, Redux Toolkit"
+  },
+  {
+    id: 2,
+    logo: job  ,
+    title: "Job Zone",
+    github: "https://github.com/MohdFahad1/jobzone",
+    demo: "https://jobzone.vercel.app/",
+    tech: "Next JS, Tailwind CSS, Jobs API"
+  },
+  {
+    id: 3,
+    logo: unicus  ,
+    title: "Unicus Mobility",
+    github: "https://github.com/MohdFahad1/e-bike",
+    demo: "https://unicus-mobility.netlify.app/",
+    tech: "React JS, CSS"
+  },
+  {
+    id: 4,
     logo: pixabay ,
     title: "Pixabay Clone",
     github: "https://github.com/MohdFahad1/pixabay-clone",
     demo: "https://react-pixabay-clone.netlify.app/",
-    tech: "React JS, Javscript, CSS"
+    tech: "React JS, CSS, Pixabay API"
   },
   {
-    id: 2,
+    id: 5,
     logo: weather,
     title: "WeatherNow",
     github: "https://github.com/MohdFahad1/WeatherNow",
     demo: "https://weather-app-mohdfahad1.vercel.app/",
-    tech: "Next JS, React JS, Tailwind CSS",
+    tech: "Next JS, Tailwind CSS, openweathermap API",
   },
   {
-    id: 3,
+    id: 6,
     logo: quote ,
     title: "Random Quote",
     github: "https://github.com/MohdFahad1/random-quote.netlify",
     demo: "https://random-quote-generator-application.netlify.app",
-    tech: "React JS, Javascript, CSS"
+    tech: "React JS,  CSS, quotes API"
   },
   {
-    id: 4,
+    id: 7,
     logo: markdown,
     title: "MarkDown Editor",
     github: "https://github.com/MohdFahad1/markdown-editor",
--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,30 +1,52 @@
-import React, { useEffect } from 'react'
-import brackets from '../../Assets/braces-fill.png'
+import React, { useEffect, useState } from 'react';
+import brackets from '../../Assets/braces-fill.png';
 import './Skills.css';
 import { data } from './data.js';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 const Skills = () => {
+  const [isResponsiveMode, setIsResponsiveMode] = useState(false);
+
   useEffect(() => {
     Aos.init();
-  }, [])
+
+    const handleResize = () => {
+      setIsResponsiveMode(window.innerWidth <= 600);
+    };
+
+    handleResize(); // Check on initial load
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Skills</h1>
+      <h1 style={{ textAlign: 'center' }}>Skills</h1>
       <div id='skills'>
         <div className='skills-para'>
-          <p data-aos="fade-right" data-aos-duration="1000">I have learned a lot of skills as a frontend developer and I am always learning something new to add a new skill to the list
+          <p
+            data-aos={isResponsiveMode ? 'fade-up' : 'fade-right'}
+            data-aos-duration='1000'
+          >
+            I have learned a lot of skills as a frontend developer and I am always learning something new to add a new skill to the list
           </p>
         </div>
         <div>
           <div className='skills-container'>
             <div className='skills-content'>
-              <h3 className='skills-title' data-aos="fade-left" data-aos-duration="1200">
-                <img src={brackets} alt="brackets"/> FrontEnd Developer
+              <h3
+                className='skills-title'
+                data-aos={isResponsiveMode ? 'fade-up' : 'fade-left'}
+                data-aos-duration='1200'
+              >
+                <img src={brackets} alt='brackets' /> FrontEnd Developer
               </h3>
               <div className='skills-info'>
-                {data.map(({id, skillName, level, skillLogo}) => {
+                {data.map(({ id, skillName, level, skillLogo }) => {
                   return (
                     <div className='skills-data' key={id}>
                       <div className='skills-blob'>
@@ -33,7 +55,7 @@ const Skills = () => {
                       <h3 className='skills-name'>{skillName}</h3>
                       <span className='skills-subtitle'>{level}</span>
                     </div>
-                  )
+                  );
                 })}
               </div>
             </div>
@@ -41,7 +63,7 @@ const Skills = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
